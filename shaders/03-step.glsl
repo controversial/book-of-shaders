bab142e7de61260e894e812cdd68b9dc0f8df6e9
Luke Taylor <luke@deentaylor.com>
2019-08-24 10:11:03 -0400

    Add animation to step experiment

--- shaders/03-step.glsl
+++ shaders/03-step.glsl
@@ -2,11 +2,16 @@
 precision mediump float;
 #endif
 
+#define PI 3.1415926
 uniform vec2 u_resolution;
+uniform float u_time;
 
 void main() {
   vec2 st = gl_FragCoord.xy / u_resolution;
-  float lightness = step(0.5, st.y);
+  float lightness = step(
+    0.5 + 0.25 * sin(u_time * .5),
+    st.y + 0.25 * sin(st.x * PI + u_time * 2.05)
+  );
   vec3 color = vec3(lightness);
   gl_FragColor = vec4(color, 1.0);
 }
