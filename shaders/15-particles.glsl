72653c4726499d89ef9e69e45606624240db9679
Luke Taylor <luke@deentaylor.com>
2019-12-21 17:46:37 -0500

    Many particles!

--- shaders/15-particles.glsl
+++ shaders/15-particles.glsl
@@ -11,11 +11,12 @@ uniform float u_time;
 float random() { return fract(sin(u_time) * 100000.); }
 float random(float seed) { return fract(sin(seed) * 100000.); }
 
+
 vec2 particlePos(float seed, float time) {
-  float this_startProgress = random(seed + .1);
-  float this_speed = random(seed + .2) * 20. + 5.;
-  float this_amplitude = random(seed + .3) * .2;
-  float this_yshift = random(seed + .4) * .5 - .25;
+  float this_startProgress = random(seed + .5);
+  float this_speed = random(seed + .6) * 20. + 5.;
+  float this_amplitude = random(seed + .7) * .15;
+  float this_yshift = random(seed + .8) * .3 - .15;
   float progress = fract(this_startProgress + (time / 4.));
 
   return vec2(
@@ -30,17 +31,22 @@ void main() {
 
   vec3 color = vec3(0., 0., 0.);
 
-  float seed = 4.;
-  float radius = random(seed) * 15. + 5.;
-  vec2 center = particlePos(seed, u_time);
+  float radius;
+  float opacity;
+  vec2 center;
+  float inCircle;
 
-  float inCircle = smoothstep(radius - 1., radius + 1., distance(gl_FragCoord.xy, center));
-
-  color = mix(
-    vec3(1., 1., 1.),
-    vec3(0., 0., 0.),
-    inCircle
-  );
+  for (float seed = 0.; seed < 100.; seed += 1.) {
+    radius = random(seed) * 15. + 5.;
+    opacity = random(seed + .1);
+    center = particlePos(seed, u_time);
+    inCircle = smoothstep(radius - 1., radius + 1., distance(gl_FragCoord.xy, center));
+    color += mix(
+      vec3(opacity, opacity, opacity),
+      vec3(0., 0., 0.),
+      inCircle
+    );
+  }
 
   gl_FragColor = vec4(color, 1.);
 }
