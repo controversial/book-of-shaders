a00df29e9e1ff8ef159e0862b25c4359f06005fb
Luke Taylor <luke@deentaylor.com>
2020-04-29 13:52:03 -0400

    Update isolines shader

--- shaders/13-isolines.glsl
+++ shaders/13-isolines.glsl
@@ -1,6 +1,10 @@
+#extension GL_OES_standard_derivatives: enable
+
 #ifdef GL_ES
 precision mediump float;
 #endif
+
+
 
 uniform vec2 u_resolution;
 uniform float u_time;
@@ -12,66 +16,44 @@ vec4 taylorInvSqrt(vec4 r){return 1.79284291400159 - 0.85373472095314 * r;}
 float snoise(vec3 v) {
   const vec2  C = vec2(1.0/6.0, 1.0/3.0) ;
   const vec4  D = vec4(0.0, 0.5, 1.0, 2.0);
-
-  // First corner
   vec3 i  = floor(v + dot(v, C.yyy) );
   vec3 x0 =   v - i + dot(i, C.xxx) ;
-
-  // Other corners
   vec3 g = step(x0.yzx, x0.xyz);
   vec3 l = 1.0 - g;
   vec3 i1 = min( g.xyz, l.zxy );
   vec3 i2 = max( g.xyz, l.zxy );
-
-  //  x0 = x0 - 0. + 0.0 * C
   vec3 x1 = x0 - i1 + 1.0 * C.xxx;
   vec3 x2 = x0 - i2 + 2.0 * C.xxx;
   vec3 x3 = x0 - 1. + 3.0 * C.xxx;
-
-  // Permutations
   i = mod(i, 289.0 );
   vec4 p = permute( permute( permute(
              i.z + vec4(0.0, i1.z, i2.z, 1.0 ))
            + i.y + vec4(0.0, i1.y, i2.y, 1.0 ))
            + i.x + vec4(0.0, i1.x, i2.x, 1.0 ));
-
-  // Gradients
-  // ( N*N points uniformly over a square, mapped onto an octahedron.)
   float n_ = 1.0/7.0; // N=7
   vec3  ns = n_ * D.wyz - D.xzx;
-
-  vec4 j = p - 49.0 * floor(p * ns.z *ns.z);  //  mod(p,N*N)
-
+  vec4 j = p - 49.0 * floor(p * ns.z *ns.z);
   vec4 x_ = floor(j * ns.z);
-  vec4 y_ = floor(j - 7.0 * x_ );    // mod(j,N)
-
+  vec4 y_ = floor(j - 7.0 * x_ );
   vec4 x = x_ *ns.x + ns.yyyy;
   vec4 y = y_ *ns.x + ns.yyyy;
   vec4 h = 1.0 - abs(x) - abs(y);
-
   vec4 b0 = vec4( x.xy, y.xy );
   vec4 b1 = vec4( x.zw, y.zw );
-
   vec4 s0 = floor(b0)*2.0 + 1.0;
   vec4 s1 = floor(b1)*2.0 + 1.0;
   vec4 sh = -step(h, vec4(0.0));
-
   vec4 a0 = b0.xzyw + s0.xzyw*sh.xxyy ;
   vec4 a1 = b1.xzyw + s1.xzyw*sh.zzww ;
-
   vec3 p0 = vec3(a0.xy,h.x);
   vec3 p1 = vec3(a0.zw,h.y);
   vec3 p2 = vec3(a1.xy,h.z);
   vec3 p3 = vec3(a1.zw,h.w);
-
-  // Normalise gradients
   vec4 norm = taylorInvSqrt(vec4(dot(p0,p0), dot(p1,p1), dot(p2, p2), dot(p3,p3)));
   p0 *= norm.x;
   p1 *= norm.y;
   p2 *= norm.z;
   p3 *= norm.w;
-
-  // Mix final noise value
   vec4 m = max(0.6 - vec4(dot(x0,x0), dot(x1,x1), dot(x2,x2), dot(x3,x3)), 0.0);
   m = m * m;
   return 42.0 * dot( m*m, vec4( dot(p0,x0), dot(p1,x1),
@@ -79,19 +61,37 @@ float snoise(vec3 v) {
 }
 
 void main() {
-  vec2 st = gl_FragCoord.xy / u_resolution;
-  //                                                   increase to go right
-  //                                                   |    | increase to go up
-  float noiseLightness = snoise(vec3(st.xy * .65 - vec2(.20, 0.65), u_time / 30.));
-  float posDiff = fract(noiseLightness * 15.);
-  float negDiff = 1. - posDiff;
-  float thickness = 0.15;
-  float lightness = (step(thickness, posDiff) * step(thickness, negDiff));
+  vec2 st = gl_FragCoord.xy / u_resolution.y;
 
-  float diag = (st.x + 1. - st.y) / 2.;
-  vec3 colorA = vec3(.98, .36, .49);
-  vec3 colorB = vec3(.41, .51, .98);
-  vec3 mixed = mix(colorA, colorB, diag * 1.25);
+  float noise = snoise(vec3(st.x, u_time / 5., st.y));
+  float gradient = distance(vec2(0., 0.), st);
+  float field = mix(noise, gradient, .9);
 
-  gl_FragColor = vec4(lightness + mixed, 1);
+  float numLines = 15.;
+  float steps = fract(field * numLines);
+  // Convert sawtooth "steps" signal (/|/|) into triangle signal (/\/\)
+  // Take the minimum of these two lines: \/
+  //                                      /\ to get triangle waves
+  float up = 2. * steps;
+  float down = 2. - up;
+  float bumps = min(up, down);
+
+  // Normalize by dividing by the derivative - get uniform lines
+  float lines = bumps / fwidth(field * numLines);
+
+  // Increase width of lines
+
+  float weight = 25.;
+  lines /= weight;
+  // Harder edges on the lines
+  lines = smoothstep(.48, .52, lines);
+  lines = clamp(lines, 0., 1.);
+  lines = 1. - lines;
+
+  float diag = (st.x + (1. - st.y)) / 2.;
+  vec3 colorA = vec3(.61, .48, 1.);
+  vec3 colorB = vec3(.47, .57, 1.);
+  vec3 colorGradient = mix(colorA, colorB, gradient);
+
+  gl_FragColor = vec4(vec3(lines * colorGradient), 1);
 }
